2fddd11e21503005cec0513c106a99c065397518
Daniel Rocha <rochadhc@gmail.com>
2021-05-18 10:42:55 -0300

    change qrcode component - include download

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -1,5 +1,41 @@
-import QRREact from 'react-qr-code'
+import QRREact from 'qrcode.react'
 
 export const QRCode = ({ value }) => {
-  return <div>{value && <QRREact value={value} />}</div>
+  const downloadQR = () => {
+    const canvas = document.getElementById('qrCodeImage')
+
+    const pngUrl = canvas
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream')
+    let downloadLink = document.createElement('a')
+    downloadLink.href = pngUrl
+    downloadLink.download = 'qrCode.png'
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+  }
+
+  return (
+    <div>
+      {value && (
+        <QRREact
+          value={value}
+          id='qrCodeImage'
+          includeMargin={true}
+          size={300}
+        />
+      )}
+
+      {value && (
+        <div className=' text-center'>
+          <a
+            className='cursor-pointer text-blue-400 hover:text-red-400'
+            onClick={downloadQR}
+          >
+            Download
+          </a>
+        </div>
+      )}
+    </div>
+  )
 }
